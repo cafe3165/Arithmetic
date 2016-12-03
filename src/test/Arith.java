15857df5a3c1ff37e3d7148bf72b15880e52dda7
Joseph <78599297@qq.com>
2016-12-03 11:13:05 +0800

    add some shuju

--- src/test/Arith.java
+++ src/test/Arith.java
@@ -1,15 +1,29 @@
 package test;
+import java.util.*;
 
 public class Arith {
 
 	public static int[][][] S=new int[10][20][2];
 	public static int[] lenS=new int [20];//
 	public static int[][] score=new int[10][2];
-	public static int[][] input=new int[10][2];
-
+	public static int[][] input=new int[10][2];	
+	public static boolean[] result=new boolean[10];
+	public static int n;
+	int rightAnswer;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		
+	//@SuppressWarnings("resource")
+	Scanner s=new Scanner(System.in)	;
+	n=s.nextInt();
+//	GenS();
+//	AddK();
+//	Cal();
+//	OutRes();
+//	Com();
+	
+	
+//		System.out.println("hello");
 	}
 
 }
