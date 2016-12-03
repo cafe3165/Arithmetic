2e3fe8c2892cf80e1175f61ab8fcb42a41e8e6f5
dtew <dtew146a2@163.com>
2016-12-03 20:33:42 +0800

    输入

--- src/test/Input.java
+++ src/test/Input.java
@@ -1,16 +1,39 @@
 package test;
 import java.util.Scanner;
 
+import static java.lang.System.in;
+
 /**
  * Input
  * Created by X on 2016/12/3.
  */
 public class Input {
     public static void Input(){
-        int num=Arith.n;
-        int input[]=new int [num];
-        Scanner s=new Scanner(System.in);
-        for(int i=0;i<num;i++)
-            input[i]=s.nextInt();
+        int num = Arith.n;
+        int input[][] = Arith.input;
+        //Pattern pattern = Pattern.compile("[0-9]+");
+        Scanner s = new Scanner(in);
+        String answer=s.nextLine();
+        String[] answerarray = answer.split(" ");
+        String temp="";
+        boolean flag=true;
+        for(int i=0;i<10;i++){
+            int t=0;
+            for(int j=0;j<answerarray[i].length();j++){
+                if(answerarray[i].charAt(j)>=48&&answerarray[i].charAt(j)<=57){
+                    temp=temp+answerarray[i].charAt(j);
+                    flag=true;
+                }else{
+                    if(flag){
+                        int n=Integer.parseInt(temp);
+                        input[i][t]=n;
+                        t++;
+                        temp="";
+                        flag=false;
+                    }
+                }
+            }
+        }
+
     }
 }
