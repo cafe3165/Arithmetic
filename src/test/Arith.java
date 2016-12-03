9dcf8804a0f3aaf139c1ff238188cd15202d5190
Joseph <78599297@qq.com>
2016-12-03 10:33:52 +0800

    init some codes

--- src/test/Arith.java
+++ src/test/Arith.java
@@ -2,6 +2,11 @@ package test;
 
 public class Arith {
 
+	public static int[][][] S=new int[10][20][2];
+	public static int[] lenS=new int [20];//
+	public static int[][] score=new int[10][2];
+	public static int[][] input=new int[10][2];
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
