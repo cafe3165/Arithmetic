3d57ae1cba1c0099ff240d3072982afbabcfd505
dtew <dtew146a2@163.com>
2016-12-04 23:48:32 +0800

    input

--- src/Jtest/InputTest.java
+++ src/Jtest/InputTest.java
@@ -1,20 +1,19 @@
+package Jtest;
+
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import test.Arith;
 import test.Input;
 
-import static org.junit.Assert.*;
-
-/**
- *
- * Created by X on 2016/12/4.
- */
 public class InputTest {
-    //输入1/2、3/4,进行测试并输出input数组里的数
+
     @Test
-    public void input(){
+    public void test() {
         Arith.n=2;
         Input.Input();
         int x,y,z,t;
+
         x=Arith.input[0][0];
         y=Arith.input[0][1];
         z=Arith.input[1][0];
@@ -26,7 +25,8 @@ public class InputTest {
         assertEquals(4, t);
 
         for(int i=0;i<Arith.n;i++){
-            System.out.println(Arith.input[i][0]+"/"+Arith.input[i][1]+" ");
+            System.out.print(Arith.input[i][0]+"/"+Arith.input[i][1]+" ");
         }
     }
+
 }
